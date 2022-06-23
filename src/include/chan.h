cb7087ef2b6815a0d5cb61c80dcaab57c5adfc69
cau2134 <cau2134@noreply.git.scatterflower.online>
2022-06-23 19:46:39 +0000

    Add functionality to filter replies (#5)
    resolves #3
    
    Co-authored-by: scatterflower <marisa@scatterflower.online>
    Co-authored-by: cow-face <caden.unnasch@gmail.com>
    Reviewed-on: https://git.scatterflower.online/scatterflower/wpmgr/pulls/5
    Co-authored-by: cau2134 <cau2134@noreply.git.scatterflower.online>
    Co-committed-by: cau2134 <cau2134@noreply.git.scatterflower.online>

--- src/include/chan.h
+++ src/include/chan.h
@@ -8,24 +8,72 @@
 
 #include <cstdint>
 
+/**
+ * @brief A class describing a reply to a thread
+ */
+class Reply {
+private:
+    int no;
+    uint64_t tim;
+    int w;
+    int h;
+    std::string filename;
+    std::string ext;
+    int fsize;
+public:
+    Reply(int no, uint64_t tim, int w, int h, std::string filename, std::string ext, int fsize);
+    /**
+     * @brief Checks if the given reply contains an image
+     *
+     * @return True if the reply contains an image, false if it does not
+     */
+    bool has_image();
+
+    /**
+     * @brief Checks if the resolution of the image attached
+     * to the given reply is greater than a given minumum
+     *
+     * @param min_width The minimum width of the image
+     * @param min_height The minimum height of the image
+     *
+     * @return True if both the image width and height
+     * are greater than the minimums, false otherwise
+     */
+    bool check_resolution(int min_width, int min_height);
+
+    /**
+     * @brief Checks if the image attached to the given
+     * reply conforms to a given aspect ratio
+     *
+     * @param aspect_ratio The aspect ratio to check against
+     * @param tolerance Determines how strict aspect ratio
+     * comparisons should be. Given as a percentage of the
+     * given aspect ratio (e.g. 0.1)
+     *
+     * @return True if the difference between the given aspect 
+     * ratio and the aspect ratio of the image is less than the
+     * absolute value of tolerance*aspect_ratio, false otherwise
+     */
+    bool check_aspect_ratio(double aspect_ratio, double tolerance);
+
+    /**
+     * @brief Checks the file extension of the image
+     * attached to the given reply
+     *
+     * @param extension The file extension to be check against (e.g ".jpg")
+     *
+     * @return True if the given file extension matches the
+     * file extension of the attached image, false otherwise
+     */
+    bool check_file_ext(std::string extension);
+
+};
+
 class Chan {
 private:
     HTTP http;
 public:
     Chan();
-
-    /**
-     * @brief A data structure describing a reply to a thread
-     */
-    struct Reply {
-        int no;
-        uint64_t tim;
-        int w;
-        int h;
-        std::string filename;
-        std::string ext;
-        int fsize;
-    };
 
     /**
      * @brief Gets the current threads from a given board
