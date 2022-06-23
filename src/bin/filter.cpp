fbfb30ab55ad8957c98ab602938de39d97877901
cow-face <caden.unnasch@gmail.com>
2022-06-23 13:33:41 -0500

    improves readability of filter implementation

--- src/bin/filter.cpp
+++ src/bin/filter.cpp
@@ -3,31 +3,19 @@
 #include <cstdlib>
 
 bool has_image(Chan::Reply reply) {
-	if (reply.tim != 0) {
-		return true;
-	}
-	return false;
+	return (reply.tim != 0);
 }
 
 bool check_resolution(Chan::Reply reply, int min_width, int min_height) {
-	if (reply.w >= min_width && reply.h >= min_height) {
-		return true;
-	}
-	return false;
+	return (reply.w >= min_width && reply.h >= min_height);
 }
 
 bool check_aspect_ratio(Chan::Reply reply, double aspect_ratio, double tolerance) {
 	double epsilon = aspect_ratio * tolerance;
-	double reply_aspect_ratio = static_cast<double>(reply.w)/reply.h;
-	if (std::abs(reply_aspect_ratio - aspect_ratio) < epsilon) {
-		return true;
-	}
-	return false;
+	double reply_aspect_ratio = (double)reply.w/(double)reply.h;
+	return (std::abs(reply_aspect_ratio - aspect_ratio) < epsilon);
 }
 
 bool check_file_ext(Chan::Reply reply, std::string extension) {
-	if (reply.ext == extension) {
-		return true;
-	}
-	return false;
+	return reply.ext == extension;
 }
