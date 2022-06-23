b4bc2f97271bab026ea1631685952da11c2b81a6
cau2134 <cau2134@noreply.git.scatterflower.online>
2022-06-23 05:44:17 +0000

    Added documentation for public member methods
    resolves #1
    
    Co-authored-by: cow-face <caden.unnasch@gmail.com>
    Reviewed-on: https://git.scatterflower.online/scatterflower/wpmgr/pulls/2
    Co-authored-by: cau2134 <cau2134@noreply.git.scatterflower.online>
    Co-committed-by: cau2134 <cau2134@noreply.git.scatterflower.online>

--- src/include/chan.h
+++ src/include/chan.h
@@ -11,6 +11,14 @@ private:
     HTTP http;
 public:
     Chan();
+
+    /**
+     * @breif Gets the current threads from a given board
+     *
+     * @param board The board to get threads from
+     *
+     * @return A vector containing the thread numbers from the given board
+     */
     std::vector<int> get_threads(std::string board);
 };
 
--- src/include/http.h
+++ src/include/http.h
@@ -17,7 +17,26 @@ private:
     static size_t write_callback(char *ptr, size_t size, 
                           size_t nmemb, void *userdata);
 public:
+    /**
+    * @brief Constructor for the HTTP class
+    * 
+    * @details Initializes the download buffer and cURL
+    *
+    * @param base_url The base url to which
+    * requests will be made
+    */ 
     HTTP(std::string base_url);
+    
+    /**
+     * @brief Fetches data from a given path
+     *
+     * @details Also empties the download buffer to be used again
+     *
+     * @param path The URL of the data to be fetched from,
+     * relative to the base path
+     *
+     * @return The data from the URL
+     */
     std::string fetch_path(std::string path);
 };
 
